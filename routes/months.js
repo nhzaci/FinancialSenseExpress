ae24f1821f9f0ab74923e10ee564b55545a11107
Zack <warrior.killz@gmail.com>
2020-05-31 20:26:23 +0800

    Added sorting for searching by date

--- routes/months.js
+++ routes/months.js
@@ -4,14 +4,13 @@ const Post = require('../models/Post');
 
 // GET year and month transactions
 router.get('/:year/:month', async (req, res, next) => {
-
     //Specify start and end-dates to find
     let start = new Date(req.params.year, req.params.month - 1, 1);
     let end = new Date(req.params.year, req.params.month, 1);
 
     //console.log("start: " + start + " end: " + end);
     try {
-        const post = await Post.find({ date: {$gte: start, $lt: end} });
+        const post = await Post.find({ date: {$gte: start, $lt: end} }).sort({date:-1});
         res.status(200).json(post);
     } catch (error) {
         next(error);
@@ -32,7 +31,15 @@ router.get('/getYears', async (req, res, next) => {
                 }}
             }}
         ]);
-        res.status(200).json(post[0].distinctYear);
+        compare = function (a,b) {
+            if (a.year < b.year) {
+                return 1;
+            } else if (a.year > b.year) {
+                return -1;
+            }
+            return 0;
+        }
+        res.status(200).json(post[0].distinctYear.sort(compare));
     } catch (error) {
         next(error);
     }
@@ -41,7 +48,7 @@ router.get('/getYears', async (req, res, next) => {
 // GET months and years
 router.get('/getMonthYears', async (req, res, next) => {
     try {
-        const post = await Post.aggregate([
+        var post = await Post.aggregate([
             { "$project": {
                 "year": {"$year": "$date" },
                 "month": {"$month": "$date"}
@@ -54,7 +61,22 @@ router.get('/getMonthYears', async (req, res, next) => {
                 }}
             }}
         ]);
-        res.status(200).json(post[0].distinctYear);
+        //Function for sorting returned month and years
+        compare = function (a,b) {
+            if (a.year < b.year) {
+                return 1;
+            } else if (a.year > b.year) {
+                return -1;
+            } else if (a.year == b.year) {
+                if (a.month < b.month) {
+                    return 1;
+                } else if (a.month > b.month) {
+                    return -1;
+                }
+            }
+            return 0;
+        }
+        res.status(200).json(post[0].distinctYear.sort(compare));
     } catch (error) {
         next(error);
     }
